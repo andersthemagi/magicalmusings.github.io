26738ea7de4421e2a6512a4e60e93b94f0a99781
magicalmusings <70292022+magicalmusings@users.noreply.github.com>
2020-10-07 13:51:13 -0700

    attempting accordion implmentation via Jekyll Plugin instead

---
+++ _plugins/manual-markdown.rb
@@ -0,0 +1,11 @@
+def render(context)
+
+    site = context.registers[:site]
+    converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
+    content = converter.convert(super)
+
+    output = <<~EOS
+        <div>
+            #{content}
+        </div>
+    EOS
